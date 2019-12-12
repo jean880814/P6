5b3925f916be8ce230e563c93a5334a75c1e88cf
Jean <422921631@qq.com>
2019-12-12 11:34:48 +0800

    P6

--- p6_2/src/main/java/com/study/p6_2/P62Application.java
+++ p6_2/src/main/java/com/study/p6_2/P62Application.java
@@ -1,13 +1,13 @@
-package com.study.p6_1;
+package com.study.p6_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class P62Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(P62Application.class, args);
     }
 
 }
