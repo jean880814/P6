07e9bb5ccf160c30c5072e1b8dc10c7bd72702d7
Jean <422921631@qq.com>
2020-02-23 17:41:58 +0800

    P6 MyBatis插件原理与Spring集成

---
+++ p6_2/src/main/java/com/study/p6_2/p62_5/interceptor/SQLInfoInterceptor.java
@@ -0,0 +1,34 @@
+package com.study.p6_2.p62_5.interceptor;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+
+import java.sql.Statement;
+import java.util.Properties;
+
+@Intercepts({@Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class})})
+public class SQLInfoInterceptor implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        System.out.println("获取到SQL语句：" + statementHandler.getBoundSql().getSql());
+        try {
+            return invocation.proceed();
+        }finally {
+            long endTime = System.currentTimeMillis();
+            System.out.println("SQL执行耗时：" + (endTime-startTime) +"ms");
+        }
+    }
+
+    @Override
+    public Object plugin(Object o) {
+        return Plugin.wrap(o, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
