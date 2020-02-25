f13cb554030227bcbd57d507f516a51fbaf89d5c
Jean <422921631@qq.com>
2020-02-25 16:38:49 +0800

    P6 并发

---
+++ p6_3/src/main/java/com/study/p6_3/P63Application.java
@@ -0,0 +1,13 @@
+package com.study.p6_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P63Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P63Application.class, args);
+    }
+
+}
---
+++ p6_3/src/test/java/com/study/p6_3/P63ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.p6_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P63ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
