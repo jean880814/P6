c0eab27b9997ed31172d5cd5a8693c2593a7f38f
Jean <422921631@qq.com>
2020-03-01 10:01:14 +0800

    P6 ThreadLocal单例

--- p6_1/src/main/java/com/study/p6_1/singleton_pattern/ThreadLocalSingleton.java
+++ p6_1/src/main/java/com/study/p6_1/singleton_pattern/ThreadLocalSingleton.java
@@ -1,5 +1,8 @@
 package com.study.p6_1.singleton_pattern;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public  class ThreadLocalSingleton {
     private static final ThreadLocal<ThreadLocalSingleton> local = new ThreadLocal<ThreadLocalSingleton>();
     private ThreadLocalSingleton(){
@@ -16,6 +19,9 @@ public  class ThreadLocalSingleton {
     }
 
     private Object readResolve(){
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        lock.unlock();
         return local.get();
     }
 }
