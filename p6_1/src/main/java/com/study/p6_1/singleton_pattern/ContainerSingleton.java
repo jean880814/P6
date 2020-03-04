7ffc048c61a19e07017dca8d78c6aac7295ef52a
Jean <422921631@qq.com>
2020-03-04 14:10:41 +0800

    P6 容器单例线程安全

--- p6_1/src/main/java/com/study/p6_1/singleton_pattern/ContainerSingleton.java
+++ p6_1/src/main/java/com/study/p6_1/singleton_pattern/ContainerSingleton.java
@@ -2,21 +2,35 @@ package com.study.p6_1.singleton_pattern;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ContainerSingleton {
-    private static Map<Class<?>, Object> map = new ConcurrentHashMap<Class<?>, Object>();
+    private static Map<Class<?>, AtomicReference<Object>> map = new ConcurrentHashMap<Class<?>, AtomicReference<Object>>();
+    private static Map<Class<?>, Object> omap = new ConcurrentHashMap<Class<?>, Object>();
 
     private ContainerSingleton() {
     }
 
-    public static ContainerSingleton getInstance(Class<?> clazz) throws IllegalAccessException, InstantiationException {
-        synchronized (map) {
-            if (!map.containsKey(clazz)) {
+    public static Object getInstanceByAomic(Class<?> clazz) throws IllegalAccessException, InstantiationException {
+        AtomicReference<Object> obj = map.get(clazz);
+        if (obj == null) {
+            AtomicReference<Object> newObj = new AtomicReference<Object>(clazz.newInstance());
+            obj = map.putIfAbsent(clazz, newObj);
+            if (obj == null) {
+                obj = newObj;
+            }
+        }
+        return obj.get();
+    }
+
+    public static Object getInstance(Class<?> clazz) throws IllegalAccessException, InstantiationException {
+        synchronized (omap) {
+            if (!omap.containsKey(clazz)) {
                 Object obj = clazz.newInstance();
-                map.put(clazz, obj);
-                return (ContainerSingleton) obj;
+                omap.put(clazz, obj);
+                return obj;
             } else {
-                return (ContainerSingleton) map.get(clazz);
+                return omap.get(clazz);
             }
         }
     }
