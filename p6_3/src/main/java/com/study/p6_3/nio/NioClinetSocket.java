3c31b7c1550615c91e8be624fc6b01e8c7bcceee
Jean <422921631@qq.com>
2020-03-14 19:42:17 +0800

    P6 nio

---
+++ p6_3/src/main/java/com/study/p6_3/nio/NioClinetSocket.java
@@ -0,0 +1,89 @@
+package com.study.p6_3.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class NioClinetSocket {
+    public static Selector selector;
+    public static SocketChannel clntChan;
+
+    static {
+        try {
+            selector = Selector.open();
+            clntChan = SocketChannel.open();
+            clntChan.configureBlocking(false);
+            clntChan.connect(new InetSocketAddress("localhost", 8080));
+            clntChan.register(selector, SelectionKey.OP_READ);
+            while (!clntChan.finishConnect()) {
+
+            }
+            System.out.println("已连接！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = NioClinetSocket.clntChan;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+        new Avca(selector, socketChannel).start();
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()){
+            String word = scanner.nextLine();
+            if (word.equals("exit")) {
+                break;
+            }
+            byteBuffer.put(word.getBytes("UTF-8"));
+            byteBuffer.flip();
+            socketChannel.write(byteBuffer);
+            byteBuffer.clear();
+        }
+        scanner.close();
+    }
+
+    static class Avca extends Thread {
+        private Selector selector;
+        private SocketChannel clntChan;
+
+        public Avca(Selector selector, SocketChannel clntChan) {
+            this.selector = selector;
+            this.clntChan = clntChan;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    selector.select();
+                    Set<SelectionKey> keys = selector.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = keys.iterator();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+                    while (keyIterator.hasNext()) {
+                        SelectionKey selectionKey = keyIterator.next();
+                        if (selectionKey.isReadable()) {
+                            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                            socketChannel.read(byteBuffer);
+                            byteBuffer.flip();
+                            byte[] bytes = new byte[byteBuffer.remaining()];
+                            if (bytes.length == 0) {
+                                break;
+                            }
+                            byteBuffer.get(bytes);
+                            System.out.println(new String(bytes));
+                            byteBuffer.clear();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ p6_3/src/main/java/com/study/p6_3/nio/NioServerSocket.java
@@ -0,0 +1,76 @@
+package com.study.p6_3.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NioServerSocket {
+    private Selector selector;
+    private ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+    public NioServerSocket(int port){
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            selector = Selector.open();
+            serverSocketChannel.bind(new InetSocketAddress(port));
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("监听端口：" + port);
+        } catch (IOException e) {
+            System.out.println("监听端口失败" + e.getStackTrace().toString());
+        }
+
+    }
+
+    public void listen(){
+        try {
+            while (true) {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey next = iterator.next();
+                    iterator.remove();
+                    process(next);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void process(SelectionKey key) throws IOException {
+        if (key.isAcceptable()) {
+            ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+            SocketChannel accept = serverSocketChannel.accept();
+            accept.configureBlocking(false);
+            System.out.println("连接的地址:" + accept.getRemoteAddress() );
+            key = accept.register(selector, SelectionKey.OP_READ);
+        } else if(key.isReadable()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            int len = socketChannel.read(buffer);
+            if (len > 0) {
+                buffer.flip();
+                String content = new String(buffer.array(), 0, len);
+                System.out.println("接收到来着" + socketChannel.getRemoteAddress() + "的信息 ：" +content);
+                key = socketChannel.register(selector, SelectionKey.OP_WRITE);
+                key.attach(content);
+            }
+        } else if (key.isWritable()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            String attachment = (String) key.attachment();
+            socketChannel.write(ByteBuffer.wrap(("输出" + attachment).getBytes()));
+            socketChannel.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NioServerSocket(8080).listen();
+    }
+}
