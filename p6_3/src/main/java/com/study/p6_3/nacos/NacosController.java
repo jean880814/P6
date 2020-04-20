1086c9d078e0359fdba1f07dece064408ecbb75a
Jean <422921631@qq.com>
2020-04-20 09:53:21 +0800

    P6 nacos & sentinel

--- p6_3/src/main/java/com/study/p6_3/nacos/NacosController.java
+++ p6_3/src/main/java/com/study/p6_3/nacos/NacosController.java
@@ -12,7 +12,6 @@ public class NacosController {
     @NacosValue(value = "${nacos.test.propertie:123}", autoRefreshed = true)
     private String testProperties;
 
-
     @GetMapping("/nacos")
     public String nacos(){
         return testProperties;
